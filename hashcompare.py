75e8367d69ee969e8aa5d076f541180a06e74629
Achilleas <achilleas@tessa>
2013-10-08 18:33:11 +0300

    COPYING -> LICENSE

--- hashcompare.py
+++ hashcompare.py
@@ -12,7 +12,7 @@ License and copying
 Copyright © 2014 Achilleas Koutsou
 This work is free. You can redistribute it and/or modify it under the
 terms of the Do What The Fuck You Want To Public License, Version 2,
-as published by Sam Hocevar. See the COPYING file for more details.
+as published by Sam Hocevar. See the LICENSE file for more details.
 
 """
 
